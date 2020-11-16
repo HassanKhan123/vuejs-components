9d5f7f8783de6008209295f00c14365e69d0324e
HassanKhan123 <hk2602809@gmail.com>
2020-11-16 22:35:13 +0500

    friend-contact component added

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">Show Details</button> 
+    <ul v-if="detailsAreVisible">
+        <li><strong>Email: </strong>{{friend.email}}</li>
+        <li><strong>Phone: </strong>{{friend.phone}}</li>
+
+    </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detailsAreVisible:false,
+            friend:{
+                    id:'1',
+                    name:'Hassan',
+                    email:'hassan@gmail.com',
+                    phone:'03243219156'
+                },
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible=!this.detailsAreVisible
+        }
+    }
+}
+</script>
