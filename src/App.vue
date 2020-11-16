f06977f328cfaa10efa4b869f15efba7b7ee74a1
HassanKhan123 <hk2602809@gmail.com>
2020-11-16 22:22:52 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+        <li></li>
+    </ul>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data(){
+        return{
+            friends:[
+                {
+                    id:'1',
+                    name:'Hassan',
+                    email:'hassan@gmail.com',
+                    phone:'03243219156'
+                },
+                {
+                    id:'2',
+                    name:'Khan',
+                    email:'khan@gmail.com',
+                    phone:'03243219156'
+                }
+            ]
+        }
+    }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
