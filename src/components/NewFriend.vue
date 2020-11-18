d4785fb2e01879d4559bf5cd01cec86bb5f10e83
HassanKhan123 <hk2602809@gmail.com>
2020-11-18 07:56:48 +0500

    NewFriend component added

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="submitHandler">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="phone" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: "",
+    };
+  },
+  emits: ["add-contact"],
+  methods: {
+    submitHandler() {
+      this.$emit("add-contact", this.name, this.email, this.phone);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
 import App from './App.vue';
-import FriendContact from './components/FriendContact.vue'
+import FriendContact from './components/FriendContact.vue';
+import NewFriend from './components/NewFriend.vue';
 
-const app = createApp(App)
-app.component('friend-contact',FriendContact)
-app.mount('#app')
+const app = createApp(App);
+app.component('friend-contact', FriendContact);
+app.component('new-friend', NewFriend);
+app.mount('#app');
